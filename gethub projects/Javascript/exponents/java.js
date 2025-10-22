18912471dea0a1bf9f09c4c31c5871048db72488
sleepingWolf44 <44sleepingwolf@gmail.com>
2025-10-22 09:28:57 -0500

    Delete old gethub projects directory

--- gethub projects/Javascript/exponents/java.js
+++
@@ -1,6 +0,0 @@
-function Submit(){
-    var Starting = document.getElementById("first").value;
-    var Second = document.getElementById("Second").value;
-    var num = Starting ** Second;
-    document.getElementById("result").textContent = num
-}
